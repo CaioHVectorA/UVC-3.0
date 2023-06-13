d75170b82bb64d57012563c4eb2bde3d5ec946d8
Caio Henrique <109510970+CaioHVectorA@users.noreply.github.com>
2023-06-13 22:14:14 +0000

    Update 3 files

--- src/components/InitialPage/Gradient.tsx
+++ src/components/InitialPage/Gradient.tsx
@@ -1,11 +1,19 @@
-import '../../styles/components/gradient.css'
-
+"use client";
+import "../../styles/components/gradient.css";
+import { ChangeTheme } from "../../utilities/functions/ChangeTheme";
 export default function GradientAndTexts() {
-    return (
-            <div className='container'>
-            <h1 className=" leading-none">DESCUBRA <br /> APROFUNDE <br /><span className="colorMain">AVENTURE-SE</span></h1>
-            <p>Explore o <span className="colorMain">Universo Vector de Contos</span>, onde diversos contos tomam forma e se entrelaçam num espaço-tempo compartilhado e incrível.</p>
-            <button>Conhecer Agora</button>
-            </div>
-    )
+  return (
+    <div className="container">
+      <h1 className=" leading-none">
+        DESCUBRA <br /> APROFUNDE <br />
+        <span className="colorMain">AVENTURE-SE</span>
+      </h1>
+      <p>
+        Explore o <span className="colorMain">Universo Vector de Contos</span>,
+        onde diversos contos tomam forma e se entrelaçam num espaço-tempo
+        compartilhado e incrível.
+      </p>
+      <button onClick={ChangeTheme}>Conhecer Agora</button>
+    </div>
+  );
 }
