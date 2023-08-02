dfac91f0f39d51aba01575732209fd8c0c08f0ba
Caio Henrique <109510970+CaioHVectorA@users.noreply.github.com>
2023-08-02 00:47:20 +0000

    csbx changes

--- src/components/Characters/Char.tsx
+++ src/components/Characters/Char.tsx
@@ -1,40 +1,80 @@
-import { fakePromise } from "@/utilities/functions/MockupCharacter"
-import { Character } from "@/utilities/functions/MockupCharacter"
-import '../../styles/components/characters.css'
-import Link from "next/link"
-import { encryptData } from "@/utilities/functions/CryptoFunctions"
-import {URLSearchParams} from 'url'
-import axios from "axios"
-import { URL_READONLY } from "@/utilities/envariables"
-const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-export function CharCaracter({ data }: {data: Character}) {
-    return (
-        <Link href={`/char/${data.Nome.toLowerCase()}`}>
-    <div className="CardCharacter bg-neutral-950" style={{borderTop: `6px solid ${data.Color}`}}>
+import { fakePromise } from "@/utilities/functions/MockupCharacter";
+import { Character } from "@/utilities/functions/MockupCharacter";
+import "../../styles/components/characters.css";
+import Link from "next/link";
+import { encryptData } from "@/utilities/functions/CryptoFunctions";
+import { URLSearchParams } from "url";
+import axios from "axios";
+import { URL_READONLY } from "@/utilities/envariables";
+import fetchData from "@/utilities/functions/FetchData";
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+export function CharCaracter({ data }: { data: Character }) {
+  return (
+    <Link href={`/char/${data.Nome.toLowerCase()}`}>
+      <div
+        className="CardCharacter bg-neutral-950"
+        style={{ borderTop: `6px solid ${data.Color}` }}
+      >
         <h4 className="cardCharacterTitle text-slate-50">{data.Nome}</h4>
-        {data.Apelido && <p className="cardCharacterText text-slate-50">"{data.Apelido}"</p>}
-    </div>
-        </Link>
-    ) 
+        {data.Apelido && (
+          <p className="cardCharacterText text-slate-50">"{data.Apelido}"</p>
+        )}
+      </div>
+    </Link>
+  );
 }
 export default async function Characters() {
-    const Characters: any[] = (await axios.get(URL_READONLY+'char')).data 
-    // const Characters: any = await fakePromise() 
-    return <div className="px-8">
-        {alphabet.map(letter => (
-            <>
-            {
-                Characters.filter(item => item.Nome.toLowerCase().startsWith(letter)).length > 0 &&
-                <h4 className=" uppercase mt-5 mb-2 font-bold text-3xl">{letter}</h4>
-                }
-            <div className="gridCardCharacters">
+  const Characters: any[] = (await fetchData(URL_READONLY + "char")).data;
+  console.log(Characters);
+  return (
+    <div className="px-8">
+      {alphabet.map((letter) => (
+        <>
+          {Characters.filter((item) =>
+            item.Nome.toLowerCase().startsWith(letter)
+          ).length > 0 && (
+            <h4 className=" uppercase mt-5 mb-2 font-bold text-3xl">
+              {letter}
+            </h4>
+          )}
+          <div className="gridCardCharacters">
             {Characters.map((char: Character, index: number) => (
-                <>
-                {char.Nome.toLowerCase().startsWith(letter) && <CharCaracter data={char}/>}
-                </>
+              <>
+                {char.Nome.toLowerCase().startsWith(letter) && (
+                  <CharCaracter data={char} />
+                )}
+              </>
             ))}
-            </div>
-            </>
-        ))}
+          </div>
+        </>
+      ))}
     </div>
+  );
 }
--- src/utilities/functions/FetchData.ts
+++ src/utilities/functions/FetchData.ts
@@ -1,7 +1,24 @@
-export default function fetchData(URL: string) {
-    return fetch(URL, { next: { revalidate: 10 } })
-        .then(res => res
-            .json())
-        .then(data => { return data })
-        .catch(err => { return err })
+type res = {
+  error?: any;
+  data: any;
+  status: number;
+};
+
+export default function fetchData(URL: string): Promise<res> {
+  return fetch(URL, { next: { revalidate: 10 } })
+    .then((res) => res.json())
+    .then((data) => {
+      return {
+        error: null,
+        data,
+        status: 200,
+      };
+    })
+    .catch((err) => {
+      return {
+        error: err,
+        data: null,
+        status: 404,
+      };
+    });
 }
