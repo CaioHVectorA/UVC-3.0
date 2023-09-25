549ae6b9528cc147b67e363686e11207306ed93b
Caio Henrique Oliveira Batista <caihebatista@gmail.com>
2023-09-25 20:35:32 -0300

    alerts for non-existing pages

--- src/components/Global/Footer.tsx
+++ src/components/Global/Footer.tsx
@@ -7,14 +7,14 @@ export default function Footer() {
         <footer>
             <img src={UVC.src} />
             <ul>
-                <Link href={'/teste'}><li className='footerItem'>Perguntas Frequentes</li></Link>
-                <Link href={'/teste'}><li className='footerItem'>Saiba Mais</li></Link>
-                <Link href={'/teste'}><li className='footerItem'>Sobre Nós</li></Link>
+                <Link href={'#'} onClick={() => alert('Essa página ainda está em desenvolvimento.')}><li className='footerItem'>Perguntas Frequentes</li></Link>
+                <Link href={'#'} onClick={() => alert('Essa página ainda está em desenvolvimento.')}><li className='footerItem'>Saiba Mais</li></Link>
+                <Link href={'#'} onClick={() => alert('Essa página ainda está em desenvolvimento.')}><li className='footerItem'>Sobre Nós</li></Link>
             </ul>
             <ul>
-                <Link href={'/teste'}><li className='footerItem'>Contato</li>
-</Link>                <Link href={'/teste'}><li className='footerItem'>Nos Apoie</li></Link>
-                <Link href={'/teste'}><li className='footerItem'>Amazon</li>
+                <Link href={'#'} onClick={() => alert('Essa página ainda está em desenvolvimento.')}><li className='footerItem'>Contato</li>
+</Link>                <Link href={'#'} onClick={() => alert('Essa página ainda está em desenvolvimento.')}><li className='footerItem'>Nos Apoie</li></Link>
+                <Link href={'#'} onClick={() => alert('Essa página ainda está em desenvolvimento.')}><li className='footerItem'>Amazon</li>
 </Link>            </ul>
         </footer>
     )
