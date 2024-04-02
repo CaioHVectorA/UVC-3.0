0d44b171cc32b747da63c35b1c08d5c7afef729c
Your Name <caihebatista@gmail.com>
2024-04-01 21:43:40 -0300

    debug f deploy t3

--- src/server/jwt.ts
+++ src/server/jwt.ts
@@ -1,12 +1,12 @@
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import { verify, sign } from "jsonwebtoken";
+import { hash, compare } from "bcrypt";
 const secretKey = process.env.SECRET_KEY || "w";
 export function generateToken(data: JSON | string) {
-  return jwt.sign(data, secretKey);
+  return sign(data, secretKey);
 }
 export function verifyToken(token: string) {
   try {
-    return jwt.verify(token, secretKey);
+    return verify(token, secretKey);
   } catch (error) {
     console.log({token, error})
     return null;
@@ -14,10 +14,10 @@ export function verifyToken(token: string) {
 }
 export async function hashPassword(password: string) {
   const saltRounds = 10;
-  return await bcrypt.hash(password, saltRounds);
+  return await hash(password, saltRounds);
 }
 
 // Verificação de uma senha em relação ao hash
 export async function comparePassword(password: string, hash: string) {
-  return await bcrypt.compare(password, hash);
+  return await compare(password, hash);
 }
